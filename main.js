fa332d6b3913f471ff2ea6136a0639deeb267710
Diego-Capu <diegoospino487@gmail.com>
2026-04-28 07:44:33 -0500

    Please enter the commit message for your changes. Lines starting

--- main.js
+++
@@ -1,7 +0,0 @@
-const date = new Date()
-document.getElementById("year").innerText = date.getFullYear()
-
-// Write your Js code here 
-
-document.querySelector('.menu-toggle').addEventListener('click', () => {
-    document.querySelector('.nav_list').classList.toggle('active');});
